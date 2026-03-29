1cb4060b80f94564cd92961108b486b522da12b9
xieliujian <377624289@qq.com>
2026-03-29 22:18:14 +0800

    feat(logging): add LogLevel enum

---
+++ Runtime/Scripts/Logging/Enums/LogLevel.cs
@@ -0,0 +1,14 @@
+namespace ST.Core.Logging
+{
+    /// <summary>
+    /// 日志级别
+    /// </summary>
+    public enum LogLevel
+    {
+        Debug = 0,
+        Info = 1,
+        Warning = 2,
+        Error = 3,
+        Exception = 4
+    }
+}
